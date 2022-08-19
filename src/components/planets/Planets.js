d9333584d016c1dee7f60ac257fae4f7091da309
mudakikwa <mudakikwastory@gmail.com>
2022-08-19 15:45:29 +0200

    wip of planet bottom

--- src/components/planets/Planets.js
+++ src/components/planets/Planets.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import "./planets.scss"
 
 export default function Planets() {
   return (
-    <div>Planets</div>
+    <div id="planets">
+          <SinglePlanet image="./images/venus.jpg" text="Venus" />
+          <SinglePlanet image="./images/earth.jpg" text="Earth" />
+          <SinglePlanet image="./images/jupiter.jpg" text="Jupiter" />
+          <SinglePlanet image="./images/mars.jpg" text="Mars" />
+          <SinglePlanet image="./images/saturn.jpg" text="Saturn" />
+          <SinglePlanet image="./images/moon.jpg" text="Earth Moon" />
+    </div>
   )
 }
+function SinglePlanet({image,text}) {
+    return <div className="d-flex single-planet">
+        <div className="rounded-circle" style={{ backgroundImage: `url("${image}")` }}></div>
+        <div className="planet-text">{text}</div>
+    </div>
+}
+
