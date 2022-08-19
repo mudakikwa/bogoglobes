594b4ed1b1d4ed289fa48839a0b0aaaa96fc008e
mudakikwa <mudakikwastory@gmail.com>
2022-08-19 14:39:38 +0200

    added the logo

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from '../../components/logo/Logo'
 import "./index.scss"
 
 export default function Home() {
   return (
     <div className="d-flex" id="home">
-      <div className="left-size"></div>
+      <div className="left-size">
+        <Logo />  
+      </div>
       <div className="right-size"></div>
     </div>
   )
