f1348837cfe1291fbb4772b1ee872454f724bd3d
mudakikwa <mudakikwastory@gmail.com>
2022-08-19 16:11:08 +0200

    finished the bottom planet animations

--- src/components/planets/Planets.js
+++ src/components/planets/Planets.js
@@ -1,22 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./planets.scss"
 
 export default function Planets() {
-  return (
-    <div id="planets">
-          <SinglePlanet image="./images/venus.jpg" text="Venus" />
-          <SinglePlanet image="./images/earth.jpg" text="Earth" />
-          <SinglePlanet image="./images/jupiter.jpg" text="Jupiter" />
-          <SinglePlanet image="./images/mars.jpg" text="Mars" />
-          <SinglePlanet image="./images/saturn.jpg" text="Saturn" />
-          <SinglePlanet image="./images/moon.jpg" text="Earth Moon" />
-    </div>
-  )
+    const [currentPlanet, setcurrentPlanet] = useState(0)
+    const data = [
+        {
+            image: "./images/venus.jpg",
+            text: "Venus"
+        },
+        {
+            image: "./images/earth.jpg",
+            text: "Earth"
+        },
+        {
+            image: "./images/jupiter.jpg",
+            text: "Jupiter"
+        },
+        {
+            image: "./images/mars.jpg",
+            text: "Mars"
+        },
+        {
+            image: "./images/saturn.jpg",
+            text: "Saturn"
+        },
+        {
+            image: "./images/moon.jpg",
+            text: "Earth Moon"
+        },
+    ]
+    return (
+        <div id="planets">
+            {data.map((item, idx) => {
+                return <SinglePlanet image={item.image} text={item.text} planets={{ currentPlanet, setcurrentPlanet }} id={idx} key={idx} />
+            })}
+        </div>
+    )
 }
-function SinglePlanet({image,text}) {
-    return <div className="d-flex single-planet">
-        <div className="rounded-circle" style={{ backgroundImage: `url("${image}")` }}></div>
-        <div className="planet-text">{text}</div>
-    </div>
+function SinglePlanet({ image, text, planets, id }) {
+    const { currentPlanet, setcurrentPlanet } = planets
+    const handleClick = () => {
+        setcurrentPlanet(id)
+    }
+    return <>
+        {(currentPlanet!==id) ? <div className="d-flex single-planet clickable" onClick={handleClick}>
+            <div className="rounded-container">
+                <div className="rounded-circle" style={{ backgroundImage: `url("${image}")` }}></div>
+            </div>
+            <div className="planet-text">{text}</div>
+        </div>
+            : <div className="single-planet planet-clicked clickable" onClick={handleClick}>
+                <div className="rounded-container">
+                    <div className="rounded-circle" style={{ backgroundImage: `url("${image}")` }}></div>
+                </div>
+                <div className="planet-text">{text}</div>
+            </div>
+        }
+    </>
 }
 
