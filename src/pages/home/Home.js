b8274afa4df8b2db5268ac2edb65b0fb49a98bbf
mudakikwa <mudakikwastory@gmail.com>
2022-08-21 14:48:20 +0200

    globe positioning

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,23 +5,27 @@ import LeftTextCounter from '../../components/leftText/LeftTextCounter'
 import LeftWebGL from '../../components/leftWebGl/LeftWebGL'
 import Logo from '../../components/logo/Logo'
 import Planets from '../../components/planets/Planets'
+import SphereGl from '../../components/sphereGl/SphereGl'
 import Textures from '../../components/textures/Textures'
 import "./index.scss"
 
 export default function Home() {
   return (
-    <div className="d-flex" id="home">
-      <div className="left-size">
-        <Logo /> 
-        <Cta />
-        <Planets /> 
+    <>
+      <SphereGl />
+      <div className="d-flex" id="home">
+        <div className="left-size">
+          <Logo />
+          <Cta />
+          <Planets />
+        </div>
+        <div className="right-size">
+          <LeftWebGL />
+          <LeftTextCounter />
+          <Arrows />
+          <Textures />
+        </div>
       </div>
-      <div className="right-size">
-        <LeftWebGL />
-        <LeftTextCounter />
-        <Arrows />
-        <Textures />
-      </div>
-    </div>
+    </>
   )
 }
